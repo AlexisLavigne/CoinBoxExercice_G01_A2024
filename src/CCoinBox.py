54c152ed72e0c7d383f4141b15ab2489bb905091
AlexisLavigne <70544766+AlexisLavigne@users.noreply.github.com>
2024-10-21 11:14:50 -0400

    Update CCoinBox.py

--- src/CCoinBox.py
+++ src/CCoinBox.py
@@ -28,6 +28,7 @@ class CCoinBox:
         self.monnaie_courante = 0
         self.vente_permise = False
         print("Réinitialisation")
+    # pourrait comment cette fonction et les tests vont passer (elle n'est pas testée)
 
     def retourne_monnaie(self):
         pieces = self.monnaie_courante
